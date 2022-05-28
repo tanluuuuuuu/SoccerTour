0629771646032de04102a1b6693a767aa0ac0dbf
20521591@gm.uit.edu.vn <84212018+tanluuuuuuu@users.noreply.github.com>
2022-05-28 14:00:48 +0700

    server commit

---
+++ server/controllers/user.js
@@ -0,0 +1,102 @@
+import { userModel, teamModel } from "../models/tourModel.js";
+import _ from "lodash";
+import jwt from "jsonwebtoken";
+
+
+export const getUserList = async (req, res) => {
+    try {
+        const allUser = await userModel.find();
+        _.remove(allUser, (user) => {
+            return user._id.equals(req.user._id);
+        });
+        res.status(200).json(allUser);
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).send(error.message);
+    }
+};
+
+export const checksignin = async (req, res) => {
+    try {
+        const { token } = req.body;
+        if (!token) {
+            console.log("Empty token");
+            return res.status(401).send("Empty token");
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await userModel.findById(decoded.id);
+
+        if (!user) {
+            console.log("No user found with this id");
+            return res.status(401).send("No user found with this id");
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+};
+
+export const signin = async (req, res) => {
+    const { userName, password } = req.body;
+
+    if (!userName || !password) {
+        res.status(400).send("Please provide username and password");
+    }
+
+    try {
+        let user = await userModel
+            .findOne({ username: userName })
+            .select("+password");
+
+        if (!user) {
+            console.log("Invalid Credentials");
+            return res.status(401).send("Invalid Credentials");
+        }
+
+        const isMatch = await user.matchPassword(password);
+
+        if (!isMatch) {
+            console.log("Invalid Credential");
+            return res.status(401).send("Invalid Credential");
+        }
+
+        const token = user.getSignedToken();
+
+        user = user.toObject();
+        _.set(user, "token", token);
+        _.unset(user, "password");
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+};
+
+export const signup = async (req, res, next) => {
+    try {
+        const { phoneNumber, country, userName, password } = req.body;
+        const registeredUser = await userModel.create({
+            phoneNumber,
+            username: userName,
+            country,
+            password,
+        });
+
+        const token = registeredUser.getSignedToken();
+        registeredUser.token = token;
+        delete registeredUser["password"];
+
+        res.status(200).json(registeredUser);
+    } catch (error) {
+        console.log(error);
+        res.status(409).json({ message: error });
+    }
+};
+
+const sendToken = (user, statusCode, res) => {
+    const token = user.getSignedToken();
+    res.status(statusCode).json(token);
+};
---
+++ server/routes/tour.js
@@ -0,0 +1,33 @@
+import express from "express";
+import {
+    getTeams,
+    createTeam,
+    getTour,
+    updateTour,
+    updateMatchData,
+    updateMatchResult,
+    getPlayers,
+    createTour,
+    getRank,
+    getRankPlayer,
+    changeTourRule,
+    acceptRegister
+} from "../controllers/tour.js";
+import { UserProtection, AdminProtection } from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.get("/", getTeams);
+router.post("/", UserProtection, createTeam);
+router.get("/calendar", getTour);
+router.post("/calendar", AdminProtection, createTour);
+router.patch("/calendar", AdminProtection, updateTour);
+router.put("/calendar/:id", AdminProtection, updateMatchData);
+router.put("/calendar/result/:id", AdminProtection, updateMatchResult);
+router.post("/players", getPlayers);
+router.get("/ranking", getRank);
+router.get("/rankingPlayer", getRankPlayer);
+router.patch("/rule", AdminProtection, changeTourRule);
+router.post("/acceptregister", AdminProtection, acceptRegister, getTour);
+
+export default router;
